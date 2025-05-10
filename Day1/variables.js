efa7eaadd89817131831e52b956c3fd762108f24
Shankar Prasad sah <shankarprasadsah66@gmail.com>
2025-05-11 00:11:22 +0530

    Create variables.js

---
+++ Day1/variables.js
@@ -0,0 +1,8 @@
+//the var keyword should only be used in code written for old browers
+let a=6
+let b=2
+console.log(a+b)
+
+let a1="51"
+let b1=5
+console.log(a1+b1)//Since a1 is a string ("51"), JavaScript treats b1 as a string as well and combines them rather than performing numeric addition.
